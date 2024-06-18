12222096c44e85cd00f867733fc7968a6e3f7145
willz4321 <willz4321@gmail.com>
2024-06-18 20:02:50 -0300

    calculadora

--- Program.cs
+++ Program.cs
@@ -12,6 +12,11 @@ class Program
 
         IGestionTareas gestionTareas = new Tarea();
 
+        Calculadora calc = new Calculadora();
+
+        string comando;
+        double numero;
+
         Console.Write($"¿Cuántas tareas desea crear?: ");
         cantidad = int.Parse(Console.ReadLine());
         
@@ -45,5 +50,49 @@ class Program
             Console.WriteLine("¿Desea realizar otra operación? (s/n)");
             opcion = Console.ReadLine();
         } while (opcion.ToLower() == "s");
+
+        do
+        {
+            Console.WriteLine("Ingrese el comando (sumar, restar, multiplicar, dividir, limpiar, historial, salir):");
+            comando = Console.ReadLine();
+
+            switch (comando)
+            {
+                case "sumar":
+                    Console.WriteLine("Ingrese el número a sumar:");
+                    numero = Convert.ToDouble(Console.ReadLine());
+                    calc.Sumar(numero);
+                    break;
+                case "restar":
+                    Console.WriteLine("Ingrese el número a restar:");
+                    numero = Convert.ToDouble(Console.ReadLine());
+                    calc.Restar(numero);
+                    break;
+                case "multiplicar":
+                    Console.WriteLine("Ingrese el número a multiplicar:");
+                    numero = Convert.ToDouble(Console.ReadLine());
+                    calc.Multiplicar(numero);
+                    break;
+                case "dividir":
+                    Console.WriteLine("Ingrese el número a dividir:");
+                    numero = Convert.ToDouble(Console.ReadLine());
+                    calc.Dividir(numero);
+                    break;
+                case "limpiar":
+                    calc.Limpiar();
+                    break;
+                case "historial":
+                    // Mostrar el historial de operaciones
+                    var historial = calc.ObtenerHistorial();
+                    foreach(var operacion in historial)
+                    {
+                        Console.WriteLine($"Resultado anterior: {operacion.ResultadoAnterior}, Nuevo valor: {operacion.NuevoValor}, Tipo de operación: {operacion.OperacionMatematica1}");
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"Resultado: {calc.Resultado}");
+
+        } while (comando != "salir");
     }
 }
